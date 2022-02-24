6682bd26b31c94d96807abbd8b66c3d9ac980986
Wanda <cwand07@gmail.com>
2022-02-23 23:05:48 -0300

    Add challenge 01

--- js/app.js
+++ js/app.js
@@ -1,26 +1,6 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Soy hacker',
-    cantidad: 0,
-    text: 'Clase 2',
-    items: '<li>Mis servicios</li>',
-    photo: 'https://www.placecage.com/200/300',
-    alt: 'Nicolas Cage',
-    key: 'title',
-    myStyle: 'color: green',
-    firstName: 'Niki',
-    lastName: 'Nudes',
-  },
-  methods: {
-    saludar() {
-      alert('Hola');
-    },
-    saludarDos(event) {
-      alert('Coordenadas del botón: ' + event.screenX + ' ' + event.screenY);
-    },
-    showName() {
-      return this.firstName + ' ' + this.lastName;
-    },
+    counter: 0,
   },
 });
